237e09496f88cf69cbafd0eb4830e936d72387f5
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2022-02-19 23:58:21 -0500

    first_commit

---
+++ Core/Inc/lcd_display.h
@@ -0,0 +1,13 @@
+/*
+ * lcd_display.h
+ *
+ *  Created on: Feb 19, 2022
+ *      Author: yaman
+ */
+
+#ifndef INC_LCD_DISPLAY_H_
+#define INC_LCD_DISPLAY_H_
+
+
+
+#endif /* INC_LCD_DISPLAY_H_ */
---
+++ Core/Src/lcd_display.c
@@ -0,0 +1,10 @@
+/*
+ * lcd_display.c
+ *
+ *  Created on: Feb 19, 2022
+ *      Author: yaman
+ */
+
+#include "lcd_display.h"
+#include "stm32l4xx_hal.h"
+
